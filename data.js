a0caa47ce779d809aa7da1eb95eb0b1bc11bc1a8
pvsr/prokofev_ds <prokofev_ds@pvsr.vpn>
2020-06-08 10:25:53 +0300

    app + json-server

---
+++ data.js
@@ -0,0 +1,22 @@
+module.exports = function() {
+
+    return {
+
+        users: [
+            {
+                id: 1000, name: 'Denis', balance: 0, email: 'mai_den@mail.ru'
+            },
+            {
+                id: 100, name: 'Test User 1', balance: 0, email: 'test@1'
+            },
+            {
+                id: 101, name: 'Test User 2', balance: 0, email: 'test@2'
+            },
+            {
+                id: 101, name: 'Test User 3', balance: 0, email: 'test@3'
+            }
+        ]
+
+    }
+
+}
